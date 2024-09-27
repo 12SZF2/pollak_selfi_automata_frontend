a539140021c439206edf381ba0d5ca56023c68cb
László Hunor <laszlo.hunor.2006@gmail.com>
2024-09-27 13:12:28 +0200

    Created basic design

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,17 @@
 </script>
 
 <template>
-  <main class="h-screen w-screen"></main>
+  <main class="h-screen w-screen items-center flex flex-col bg-gradient-to-t from-[#424144] to-[#ffffff] ">
+    <h1 class="text-5xl mt-8 flex">Selfie automata</h1>
+    <div class="flex items-center justify-center border-2 border-slate-800 h-1/2 w-full md:w-1/2 bg-white mt-36">
+      <div class="">
+          <p class="text-3xl">Selfie előnézet...</p>  
+      </div>
+    </div>
+    <div class="flex-row text-3xl mt-4">
+    <input type="button" value="Újra" class="">
+    <input type="button" value="Filterek" class="ml-10">
+    <input type= "button" value="Tovább" class="ml-10">
+  </div>
+    </main>
 </template>
