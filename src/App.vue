d9b6206e29cf59f37e4c9b4a6f351520c484145f
schwarczotto7 <schwarcz.yoda@gmail.com>
2024-10-01 14:45:54 +0200

    Logó, véglegesítés

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,22 @@
 </script>
 
 <template>
-  <main class="h-screen w-screen items-center flex flex-col bg-gradient-to-t from-[#424144] to-[#ffffff] ">
-    <h1 class="text-5xl mt-8 flex">Selfie automata</h1>
+  <main class="h-screen w-screen items-center flex flex-col bg-gradient-to-t from-[#424144] to-[#ffffff]">
+    <div class="size-32 top-1 absolute left-2 cursor cursor-pointer">
+      <img src="../pollak_selfie_logo.png" alt="logo">
+    </div>
+    <div class="place-content-start">
+      <h1 class="text-5xl mt-8">Selfie automata</h1>
+    </div>
     <div class="flex items-center justify-center border-2 border-slate-800 h-1/2 w-full md:w-1/2 bg-white mt-36">
       <div class="">
           <p class="text-3xl">Selfie előnézet...</p>  
       </div>
     </div>
     <div class="flex-row text-3xl mt-4">
-    <input type="button" value="Újra" class="">
-    <input type="button" value="Filterek" class="ml-10">
-    <input type= "button" value="Tovább" class="ml-10">
+    <input type="button" value="Újra" class="bg-orange-400 border-2 border-stone-950 h-14 w-32 rounded-xl cursor-pointer">
+    <input type="button" value="Filterek" class="ml-10 bg-orange-400 border-2 border-stone-950 h-14 w-36 rounded-xl cursor-pointer">
+    <input type= "button" value="Tovább" class="ml-10 bg-orange-400 border-2 border-stone-950 h-14 w-32 rounded-xl cursor-pointer">
   </div>
     </main>
 </template>
