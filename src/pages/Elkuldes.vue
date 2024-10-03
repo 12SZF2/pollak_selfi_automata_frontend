2ab6d35456a474dd3f0e0a8e3101de835860a761
CristinaiRunaldo <106381446+CristinaiRunaldo@users.noreply.github.com>
2024-10-03 13:11:55 +0200

    routerlink done

--- src/pages/Elkuldes.vue
+++ src/pages/Elkuldes.vue
@@ -8,7 +8,7 @@
     </div>
     <div class="text-3xl mt-4 items-center justify-center flex-col flex">
         <p class="text-lg">Írd be az E-mail címed és elküldjük a Selfie-det</p>
-        <input type="text" value="" placeholder="E-mail-cím" class="bg-slate-300 rounded-xl mt-3 text-lg w-full h-10">
+        <input type="text" value="" placeholder="E-mail-cím" class="bg-slate-300 rounded-xl mt-3 text-lg w-full h-10 font-semibold">
         <input type="button" value="Selfie elküldése" class="bg-orange-400 border-2 border-stone-950 h-14 w-2/3 rounded-2xl cursor-pointer mt-3">
     </div>
 </div>
--- src/pages/Szerkeszto.vue
+++ src/pages/Szerkeszto.vue
@@ -12,7 +12,9 @@ import { RouterLink } from 'vue-router';
     <div class="flex-row text-3xl mt-4">
     <input type="button" value="Újra" class="bg-orange-400 border-2 border-stone-950 h-14 w-32 rounded-xl cursor-pointer">
     <input type="button" value="Filterek" class="ml-10 bg-orange-400 border-2 border-stone-950 h-14 w-36 rounded-xl cursor-pointer">
+    <RouterLink to="/elkuldes">
     <input type= "button" value="Tovább" class="ml-10 bg-orange-400 border-2 border-stone-950 h-14 w-32 rounded-xl cursor-pointer">
+  </RouterLink>
   </div>
 
 </template>
